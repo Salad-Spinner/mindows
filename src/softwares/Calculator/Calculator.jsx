25b1d632c4ded33f21fa3555a4cfce69a73d2684
Salad_Spinner <149922091+Salad-Spinner@users.noreply.github.com>
2025-04-26 12:26:52 +1000

    Calculator app Fixed
    I added the actual calculator app bit.

--- src/softwares/Calculator/Calculator.jsx
+++ src/softwares/Calculator/Calculator.jsx
@@ -1,19 +1,60 @@
-import { VscDebug } from 'react-icons/vsc';
+import React, { useState } from 'react';
 import './Calculator.scss';
 
-const Calculator = () => (
-  <div className="Calculator">
-    <div className="Calculator-leftbar">
-      <VscDebug />
-      <p>Oops! Mindows must be broken!!</p>
-    </div>
-    <div className="Calculator-rightbar">
-      <div>
-        <p>There seems to be no Calculator installed on the system.</p>
-        <p>But wait... you can always add it and send a pull request.</p>
+const Calculator = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleClick = (value) => setInput(input + value);
+
+  const calculate = () => {
+    try {
+      setResult(eval(input)); // Note: Using eval is not recommended for untrusted input
+    } catch {
+      setResult('Error');
+    }
+  };
+
+  const clear = () => {
+    setInput('');
+    setResult('');
+  };
+
+  return (
+    <div className="Calculator">
+      <div className="Calculator-leftbar">
+        <input type="text" value={input} readOnly />
+        <p>{result}</p>
+      </div>
+      <div className="Calculator-rightbar">
+        <div>
+          <button onClick={() => handleClick('1')}>1</button>
+          <button onClick={() => handleClick('2')}>2</button>
+          <button onClick={() => handleClick('3')}>3</button>
+          <button onClick={() => handleClick('+')}>+</button>
+        </div>
+        <div>
+          <button onClick={() => handleClick('4')}>4</button>
+          <button onClick={() => handleClick('5')}>5</button>
+          <button onClick={() => handleClick('6')}>6</button>
+          <button onClick={() => handleClick('-')}>-</button>
+        </div>
+        <div>
+          <button onClick={() => handleClick('7')}>7</button>
+          <button onClick={() => handleClick('8')}>8</button>
+          <button onClick={() => handleClick('9')}>9</button>
+          <button onClick={() => handleClick('*')}>*</button>
+        </div>
+        <div>
+          <button onClick={clear}>C</button>
+          <button onClick={() => handleClick('0')}>0</button>
+          <button onClick={calculate}>=</button>
+          <button onClick={() => handleClick('/')}>/</button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Calculator;
+
